622b5e70a7776ad4ffad11320ced9fc175307d20
divyakhandelwa <35268473+divyakhandelwa@users.noreply.github.com>
2018-01-11 00:01:26 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter year\n");
+    scanf("%d",&n);
+    if(n%4==0)
+   { printf("yes");
+   }
+    else
+       {
+       printf("no");
+    }
+    }
