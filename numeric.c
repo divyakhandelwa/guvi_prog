cb4bc0778bbffb493e664bf6fcf45a0314ec4d52
divyakhandelwa <35268473+divyakhandelwa@users.noreply.github.com>
2018-01-16 22:06:44 +0530

    Add files via upload

---
+++ numeric.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int i=0,len,f=0;
+char s[10];
+    scanf("%s",&s);
+    len=strlen(s);
+    for(i=0;i<len;i++)
+    {
+    if(s[i]=='0' || s[i]=='1' || s[i]=='2' || s[i]=='3' || s[i]=='4'  || s[i]=='5' || s[i]=='6' || s[i]=='7' || s[i]=='8' ||s[i]=='9')
+     {
+       f=1;
+     }
+
+    }
+    if(f==1)
+    {
+        printf("yes");
+    }
+
+
+}
+
+
