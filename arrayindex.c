2074c4fd656c1810412b95f0becd1b1f6d56530b
divyakhandelwa <35268473+divyakhandelwa@users.noreply.github.com>
2018-01-18 22:31:10 +0530

    Add files via upload

---
+++ arrayindex.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{ int a[50],n,i;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+  { scanf("%d",&a[i]);
+            }
+   for(i=0;i<n;i++)
+   {  printf("%d%d\n",a[i],i);
+}
+}
