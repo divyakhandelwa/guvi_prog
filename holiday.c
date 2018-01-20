0af7373c54c565575f6e914865ba3037f2117b67
divyakhandelwa <35268473+divyakhandelwa@users.noreply.github.com>
2018-01-20 21:25:51 +0530

    Add files via upload

---
+++ holiday.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    char str[10],str1[10];
+scanf("%s%s",&str,&str1);
+if(str>='monday' && str<='friday')
+{
+printf("no\n");
+}
+else
+{
+printf("yes\n");
+}
+if(str1>='monday' && str1<='friday')
+{
+printf("no\n");}
+else
+{
+printf("yes\n");
+}
+}
